19dbe235bb9465713edf03f690405d184d0d5335
tejashree24 <tejashreedhole24@gmail.com>
2018-07-10 17:39:56 +0530

    tried example of class-binding

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -2,13 +2,36 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `<h2> Welcome to {{name}}</h2>
+  			 <h2 class="text-success"> Simple CLass Example</h2>
+  			 <h2 [class]="successClass"> Class Binding Example</h2>
+  			 <h2 [class.text-danger]="hasError"> Class Has Error </h2>
+  			 <h2 [ngClass]="messageClasses"> Multiple Class example with ng class</h2>`,
+
+  styles: [`.text-success{
+  			color:green;
+  			},
+  			.text-danger{
+  			color:red;
+  			},
+  			.text-special{
+  			font-style:italic;
+  			}
+  		`]
 })
+
 export class TestComponent implements OnInit {
+	public name="Angular 6";
+	public successClass="text-success"; 
+	public hasError = false;
+	public isSpecial = true;
 
+	public messageClasses = {
+		"text-success": !this.hasError,
+		"text-danger": this.hasError,
+		"text-special": this.isSpecial 
+	}
   constructor() { }
-
   ngOnInit() {
   }
 
