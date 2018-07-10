54769a1c6edd072cef5b165996f7278ff4dbb7b5
tejashree24 <tejashreedhole24@gmail.com>
2018-07-10 19:40:34 +0530

    tried example for template-reference-variable

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -2,14 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `<h6>Template Reference Variable</h6>
+  			<input #myMsg type="text">
+  			<button (click)="myMessage(myMsg.value)">Click</button>`,
+  styles: []
 })
 export class TestComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  myMessage(value){
+  	console.log(value);
   }
 
 }
