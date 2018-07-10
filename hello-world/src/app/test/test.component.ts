ab5b5262072376ead52c4bd514055de169d1be6f
tejashree24 <tejashreedhole24@gmail.com>
2018-07-10 18:39:56 +0530

    tried examples for style-binding

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -2,11 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `<h2 [style.color]="hasError ? 'red' : 'green'">Style Binding 1</h2>
+  			<h3 [style.color]="highLightColor">Style Binding 2</h3>
+  			<h3 [ngStyle]="titleStyles">Style Binding 3</h3>`,
+  styles: []
 })
 export class TestComponent implements OnInit {
-
+	public name="Angular6";
+	public hasError = true;
+	public highLightColor= "orange";
+	public titleStyles = {
+	color: "blue",
+	fontStyle : "italic"
+	}
   constructor() { }
 
   ngOnInit() {
