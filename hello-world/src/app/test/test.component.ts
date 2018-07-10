9c42e38a6a56462bb3430098bc54a85b0a6e4178
tejashree24 <tejashreedhole24@gmail.com>
2018-07-10 19:22:40 +0530

    tried example for event-binding

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -2,10 +2,28 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `<h2> Welcome to {{name}}</h2>
+  			<p>*** With Handler ***</p>
+  			<button (click)="onClick($event)">Greet</button>
+  			<span>Type of Event:</span> {{eventtype}}
+  			<br><br>
+  			<p>***Without Handler***</p>
+  			<button (click)="data ='Welcome to Angular 6 Event Binding without handler'">greet</button>
+  			{{data}}`,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
+	public name="Event Binding!";
+	public data="";
+	public greeting = "";
+
+	onClick(event){
+		console.log(event);
+
+		console.log("clicked on Button!");
+		this.greeting="greetings are here!";
+		this.eventtype=event.type;
+	}
 
   constructor() { }
 
