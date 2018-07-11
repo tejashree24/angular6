b0351042d67b5deda5037c77e1b84af474c80ca7
tejashree24 <tejashreedhole24@gmail.com>
2018-07-11 10:51:36 +0530

    tried example for two way binding

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `<input [(ngModel)]="name" type="text">
+  			{{name}}
+  			`,
+  styles: []
 })
 export class TestComponent implements OnInit {
-
+  public name="";
   constructor() { }
 
   ngOnInit() {
